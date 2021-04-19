89a88ae42cb69475f18cfa15ff881138f47bacc2
Stuart Chapman <StuBez@users.noreply.github.com>
2021-04-19 14:14:56 +0100

    Upgrade Gatsby

--- gatsby/src/componenets/SocialIcon.js
+++ gatsby/src/componenets/SocialIcon.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default props => (
+const SocialIcon = props => (
     <a href={props.href} title={props.title} className="mx-3 my-2">
         <svg viewBox="0 0 24 24" className="fill-current h-10" style={{color: props.color}}>
             <path d={props.path}/>
         </svg>
     </a>
 );
+
+export default SocialIcon
