720f079d6d39ecc64991d60feed0c28fa1f5c567
Stuart Chapman <StuBez@users.noreply.github.com>
2021-04-19 21:33:22 +0100

    Load environment specific env vars

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -4,6 +4,20 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+});
+
+console.log({
+    resolve: "gatsby-source-sanity",
+    options: {
+        projectId: "7jhacyva",
+        dataset: "links",
+        token: process.env.SANITY_TOKEN,
+        watchMode: !!process.env.SANITY_WATCH_MODE,
+        overlayDrafts: !!process.env.SANITY_OVERLAY_DRAFTS,
+    },
+})
 module.exports = {
     /* Your site config here */
     plugins: [
