a754d89a0d7de329c643730e0b4d563af639b853
Stuart Chapman <StuBez@users.noreply.github.com>
2021-04-20 09:00:12 +0100

    Remove debugging

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -8,16 +8,6 @@ require("dotenv").config({
     path: `.env.${process.env.NODE_ENV}`,
 });
 
-console.log({
-    resolve: "gatsby-source-sanity",
-    options: {
-        projectId: "7jhacyva",
-        dataset: "links",
-        token: process.env.SANITY_TOKEN,
-        watchMode: !!process.env.SANITY_WATCH_MODE,
-        overlayDrafts: !!process.env.SANITY_OVERLAY_DRAFTS,
-    },
-})
 module.exports = {
     /* Your site config here */
     plugins: [
